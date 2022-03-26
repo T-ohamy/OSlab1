16167dc3f6c838fb46d6677d8e3164b1ef4fc064
T-ohamy <33750762+T-ohamy@users.noreply.github.com>
2022-03-26 19:41:19 +0200

    Add files via upload

---
+++ shell.h
@@ -0,0 +1,20 @@
+#ifndef _SHELL_C_INCLUDED_
+#include "../builtInFunctions/builtInFunctions.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define TOKEN_BUFFER_SIZE 64
+#define TOKEN_DELIMTER " \t\r\n\a"
+
+void getJobDone();
+char* readLine();
+char** readArgs(char* line);
+int launchShell(char** args);
+int executeShell(char** args);
+
+#include "shell.c"
+#define  _SHELL_C_INCLUDED_
+#endif
